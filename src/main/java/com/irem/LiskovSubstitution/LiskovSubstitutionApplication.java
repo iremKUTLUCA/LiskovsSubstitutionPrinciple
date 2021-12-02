7a43c3770db2791c4c563064a9a22e78413fb667
iremKUTLUCA <irm.ktlc@gmail.com>
2021-12-03 00:58:39 +0300

    ilk gonderi

---
+++ src/main/java/com/irem/LiskovSubstitution/LiskovSubstitutionApplication.java
@@ -0,0 +1,13 @@
+package com.irem.LiskovSubstitution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiskovSubstitutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiskovSubstitutionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/irem/LiskovSubstitution/shape/Shape.java
@@ -0,0 +1,5 @@
+package com.irem.LiskovSubstitution.shape;
+
+public interface Shape {
+    long area();
+}
